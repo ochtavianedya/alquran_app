eedbd3cdd33eff045ea26aee073c5e9517a19b67
Ochtavian Edya <edyapratama712@gmail.com>
2025-03-19 12:34:23 +0700

    modify in surah detail view and controller file

--- lib/app/modules/surah_detail/controllers/surah_detail_controller.dart
+++ lib/app/modules/surah_detail/controllers/surah_detail_controller.dart
@@ -1,10 +1,18 @@
 import 'dart:convert';
 
+import 'package:alquran_app/app/constants/theme.dart';
 import 'package:alquran_app/app/data/models/surah_detail.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class SurahDetailController extends GetxController {
+  RxBool isDark = false.obs;
+
+  void toggleTheme() {
+    isDark.value = !isDark.value;
+    Get.changeTheme(isDark.value ? appDark : appLight);
+  }
+
   Future<SurahDetail> getSurahDetail(String id) async {
     Uri url = Uri.parse('https://api.quran.gading.dev/surah/$id');
     var res = await http.get(url);
--- lib/app/modules/surah_detail/views/surah_detail_view.dart
+++ lib/app/modules/surah_detail/views/surah_detail_view.dart
@@ -1,3 +1,4 @@
+import 'package:alquran_app/app/constants/color.dart';
 import 'package:alquran_app/app/data/models/surah.dart';
 import 'package:alquran_app/app/data/models/surah_detail.dart' as surah_detail;
 import 'package:flutter/material.dart';
@@ -15,33 +16,93 @@ class SurahDetailView extends GetView<SurahDetailController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Surah ${surah.name.transliteration.id}",
-          style: GoogleFonts.poppins(color: Color(0xff672CBC)),
-        ),
+        title: Text("Surah ${surah.name.transliteration.id}"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Get.find<SurahDetailController>().toggleTheme();
+            },
+            icon: Obx(
+              () =>
+                  Get.find<SurahDetailController>().isDark.value
+                      ? Icon(Icons.light_mode_outlined)
+                      : Icon(Icons.dark_mode_outlined),
+            ),
+          ),
+        ],
       ),
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: [
-          Card(
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Text(
-                    surah.name.transliteration.id,
-                    style: GoogleFonts.poppins(fontSize: 26),
+          GestureDetector(
+            onTap:
+                () => Get.dialog(
+                  Dialog(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(25),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "Tafsir ${surah.name.transliteration.id}",
+                            style: GoogleFonts.poppins(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            surah.tafsir.id,
+                            style: GoogleFonts.poppins(fontSize: 14),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  Text(
-                    "( ${surah.name.translation.id} )",
-                    style: GoogleFonts.poppins(fontSize: 16),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    "${surah.revelation.id} | ${surah.numberOfVerses} Ayat",
-                    style: GoogleFonts.poppins(fontSize: 14),
-                  ),
-                ],
+                ),
+            child: Container(
+              width: Get.width,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [primaryColorLight, primaryColorDark],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Text(
+                      surah.name.transliteration.id,
+                      style: GoogleFonts.poppins(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "( ${surah.name.translation.id} )",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "${surah.revelation.id} | ${surah.numberOfVerses} Ayat",
+                      style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -67,7 +128,11 @@ class SurahDetailView extends GetView<SurahDetailController> {
                   return Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Card(
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: primaryColorLight.withValues(alpha: 0.1),
+                        ),
                         child: Padding(
                           padding: const EdgeInsets.symmetric(
                             vertical: 5,
@@ -76,13 +141,24 @@ class SurahDetailView extends GetView<SurahDetailController> {
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              CircleAvatar(
-                                backgroundColor: Color(0xff672CBC),
-                                child: Text(
-                                  "${index + 1}",
-                                  style: GoogleFonts.poppins(
-                                    fontSize: 14,
-                                    color: Colors.white,
+                              Obx(
+                                () => Container(
+                                  width: 45,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                        controller.isDark.isTrue
+                                            ? 'assets/images/list_dark.png'
+                                            : 'assets/images/list_light.png',
+                                      ),
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "${index + 1}",
+                                      style: GoogleFonts.poppins(fontSize: 14),
+                                    ),
                                   ),
                                 ),
                               ),
@@ -91,17 +167,9 @@ class SurahDetailView extends GetView<SurahDetailController> {
                                   IconButton(
                                     onPressed: () {},
                                     icon: const Icon(
-                                      Icons.share,
-                                      size: 24.0,
-                                      color: Color(0xff672CBC),
-                                    ),
-                                  ),
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
                                       Icons.play_arrow,
                                       size: 24.0,
-                                      color: Color(0xff672CBC),
+                                      color: purpleColor,
                                     ),
                                   ),
                                   IconButton(
@@ -109,7 +177,7 @@ class SurahDetailView extends GetView<SurahDetailController> {
                                     icon: const Icon(
                                       Icons.bookmark_border_outlined,
                                       size: 24.0,
-                                      color: Color(0xff672CBC),
+                                      color: purpleColor,
                                     ),
                                   ),
                                 ],
@@ -122,10 +190,10 @@ class SurahDetailView extends GetView<SurahDetailController> {
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 5),
                         child: Text(
-                          "${ayat.text.arab}",
+                          ayat.text.arab,
                           textAlign: TextAlign.end,
                           style: GoogleFonts.amiri(
-                            fontSize: 22,
+                            fontSize: 20,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -136,9 +204,12 @@ class SurahDetailView extends GetView<SurahDetailController> {
                           vertical: 5,
                           horizontal: 10,
                         ),
-                        child: Text(
-                          "${ayat.translation.id}",
-                          style: GoogleFonts.poppins(fontSize: 14),
+                        child: Container(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            ayat.translation.id,
+                            style: GoogleFonts.poppins(fontSize: 14),
+                          ),
                         ),
                       ),
                       const SizedBox(height: 30),
